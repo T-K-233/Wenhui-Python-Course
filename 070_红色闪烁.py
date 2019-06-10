5e3bcae63788123a907a6e1debfab8790e826811
-T.K.- <tk.fantasy.233@gmail.com>
2019-06-10 15:34:33 +0800

    update

--- 070_红色闪烁.py
+++ 070_红色闪烁.py
@@ -1,34 +1,38 @@
 """ 070_红色闪烁.py
-    我们以前用的是颜色字符串来表示颜色，在python海龟画图中我们能用三个从0到255的数值来表示一种颜色。
-    例：(255,0,0)表示的是红色，(0,255,0)表示的是绿色，(0,0,255)表示的是蓝色。
-    用这种方法表示颜色叫RGB表示法，R即red，代表红色。G即green代表绿色。B即Blue代表蓝色。
-    在计算机中所有的颜色都是用RGB三种颜色合成的。
-    我们把RGB三种颜色都分成256份。让它们的值从0到255，如果R的值为0的话，表示红色份量为0。
-    即没有红色，当红色的份量一直增大到最后为255的时候，就是红色最红的时候。
-    当G的值为0的话，表示绿色份量为0，即没有绿色，G的值一直增大，就会让绿色的份量越来越多。
-    当B的值越来越大，蓝色的份量也是越来越多。
+    我们以前用的是颜色字符串来表示颜色，在 python 海龟画图中我们可以用三个 0~255 的数值来表示一种颜色。
+    例：(255, 0, 0) 表示的是红色，(0, 255, 0) 表示的是绿色，(0, 0, 255) 表示的是蓝色。
+    用这种方法表示颜色叫 RGB 表示法，在计算机中所有的颜色都是用RGB三种颜色合成的。
+    我们把RGB三种颜色都分成256份。让它们的值从 0 到 255，如果R的值为 0 的话，表示红色份量为 0 。
+    即没有红色，当红色的份量一直增大到最后为255的时候，就是红色最大的时候。
+    绿色和蓝色同理。
     本程序演示的是红色的份量越来越多，小海龟越来越红，它的其它颜色份量都为0。
-    如果R和G的份量同时越来越多，小海龟的颜色会怎么样呢？（把B份量设为0)
 """
 from turtle import *
 bgcolor("black")
 
-红龟 = Turtle(visible=False)
-#红龟.color('black','black')      #分别设定海龟的画笔颜色和填充颜色
-红龟.shapesize(5)                #让海龟变大为原来的5倍
-红龟.penup()
-红龟.getscreen().colormode(255)
-红龟.goto(-100, 0)
-红龟.showturtle()
+red_shiny_turtle = Turtle(visible=False)
+
+red_shiny_turtle.shape("arrow")                 # 上节课问题，修改海龟形状，可以是“arrow”, “turtle”, “circle”, “square”, “triangle”, “classic”
+
+red_shiny_turtle.getscreen().colormode(255)     # 设定颜色最大值为 255
+
+red_shiny_turtle.shapesize(5, 5)                # 让海龟变大为原来的 5 倍
+red_shiny_turtle.penup()
+
+red_shiny_turtle.goto(-100, 0)
+red_shiny_turtle.showturtle()
+
 i = 0
-def 发光():
-    global i                     #声明i是全局变量，即这里的i是函数外面定义的i  
-    红龟.fillcolor(i, 0, 0)        #设定龟的填充颜色为(i,0,0)
+
+def shine():
+    global i                                    # 声明 i 是全局变量，即这里的i是函数外面定义的 i  
+    red_shiny_turtle.fillcolor(i, 0, 0)         # 设定龟的填充颜色为 (i, 0, 0)
     i = i + 1
-    i = i % 256                    #对255取余，因为每种颜色份量的最大值只能是255
+    i = i % 256                                 # 对 255 取余，因为每种颜色份量的最大值只能是 255
     title("当前RGB值：(" + str(i) + ", 0, 0)")
-    ontimer(发光, 1)             #每10毫秒调用一次发光这个函数  
-发光()
+    ontimer(shine, 1)                           # 每 10 毫秒调用一次发光这个函数  
+
+shine()
 
  
 
--- 071_颜色混合器.py
+++ 071_颜色混合器.py
@@ -1,6 +1,6 @@
-""" 072_颜色混合器.py
+""" 071_颜色混合器.py
 
-     本程序也能做为RGB颜色展示用途。  
+    展示RGB颜色混合。
 """
 from turtle import *
 r = 0
@@ -9,57 +9,90 @@ b = 0
 
 bgcolor("black")
 
-红龟 = Turtle(visible=False)
-红龟.getscreen().colormode(255)
-红龟.color('gray100', 'red')      # 分别设定海龟的画笔颜色和填充颜色
-红龟.shapesize(5)                 # 让海龟变大为原来的5倍
-红龟.penup()
-红龟.goto(-100, 0)
-红龟.showturtle()
+red = Turtle(visible=False)
+red.getscreen().colormode(255)
+red.color('gray100', 'red')      # 分别设定海龟的画笔颜色和填充颜色
+red.shapesize(5)                 # 让海龟变大为原来的5倍
+red.penup()
+red.goto(-100, 0)
+red.showturtle()    
 
-def 红色变换(x, y):       #作为ondrag的事件处理函数，它一定要有x和y参数，接收鼠标的x,y坐标。   
+
+green = Turtle(visible=False)
+green.getscreen().colormode(255)
+green.color('gray100', 'green')
+green.shapesize(5)
+green.penup()
+green.goto(0, 0)
+green.showturtle()
+
+
+blue = Turtle(visible=False)
+blue.getscreen().colormode(255)
+blue.color('gray100', 'blue')
+blue.shapesize(5)
+blue.penup()
+blue.goto(100, 0)
+blue.showturtle()
+
+
+def red_plus():
     global r, g, b
-    r = max(0, min(y, 255))           #限定r的范围为0到255。
-    红龟.sety(r)
+    r += 5
+    r = max(0, min(r, 255))         # 限定r的范围为0到255。
+    red.sety(r)
+    bgcolor(r, g, b)
+    title("当前RGB值：(" + str(r) + ","  + str(g) + "," + str(b) + ")")
+
+def red_minus():
+    global r, g, b
+    r -= 5
+    r = max(0, min(r, 255))
+    red.sety(r)
+    bgcolor(r, g, b)
+    title("当前RGB值：(" + str(r) + ","  + str(g) + "," + str(b) + ")")
+
+def green_plus():
+    global r, g, b
+    g += 5
+    g = max(0, min(g, 255))
+    green.sety(g)
+    bgcolor(r, g, b)
+    title("当前RGB值：(" + str(r) + ","  + str(g) + "," + str(b) + ")")
+
+def green_minus():
+    global r, g, b
+    g -= 5
+    g = max(0, min(g, 255))
+    green.sety(g)
     bgcolor(r, g, b)
     title("当前RGB值：(" + str(r) + ","  + str(g) + "," + str(b) + ")")
     
-
-
-绿龟=Turtle(visible=False)
-绿龟.color('gray100',(0,255,0))      #分别设定海龟的画笔颜色和填充颜色
-绿龟.shapesize(5)                #让海龟变大为原来的5倍
-绿龟.penup()
-绿龟.goto(0,0)
-绿龟.showturtle()
-
-def 绿色变换(x,y):       #作为ondrag的事件处理函数，它一定要有x和y参数，接收鼠标的x,y坐标。   
-    global r,g,b
-    g=max(0,min(y,255))           #限定r的范围为0到255。
-    绿龟.sety(g)
-    bgcolor(r,g,b)
+def blue_plus():
+    global r, g, b
+    b += 5
+    b = max(0, min(b, 255))
+    blue.sety(b)
+    bgcolor(r, g, b)
     title("当前RGB值：(" + str(r) + ","  + str(g) + "," + str(b) + ")")
 
-蓝龟 = Turtle(visible=False)
-蓝龟.color('gray100','blue')      #分别设定海龟的画笔颜色和填充颜色
-蓝龟.shapesize(5)                #让海龟变大为原来的5倍
-蓝龟.penup()
-蓝龟.goto(100, 0)
-蓝龟.showturtle()
-
-def 蓝色变换(x, y):       #作为ondrag的事件处理函数，它一定要有x和y参数，接收鼠标的x,y坐标。   
+def blue_minus():
     global r, g, b
-    b = max(0, min(y, 255))           #限定r的范围为0到255。
-    蓝龟.sety(b)   
+    b -= 5
+    b = max(0, min(b, 255))
+    blue.sety(b)
     bgcolor(r, g, b)
-    title("当前RGB值：(" + str(r) +  ","  + str(g) + "," + str(b) + ")")
-    
-    
+    title("当前RGB值：(" + str(r) + ","  + str(g) + "," + str(b) + ")")
+
+screen = red.getscreen()
+screen.onkeypress(red_minus, "e")
+screen.onkeypress(red_plus, "r")
+screen.onkeypress(green_minus, "f")
+screen.onkeypress(green_plus, "g")
+screen.onkeypress(blue_minus, "v")
+screen.onkeypress(blue_plus, "b")
 
 
-红龟.ondrag(红色变换)   #拖动事件发生时，调用红色变换函数，这个函数会接受鼠标的坐标。
-绿龟.ondrag(绿色变换)   #拖动事件发生时，调用红色变换函数，这个函数会接受鼠标的坐标。
-蓝龟.ondrag(蓝色变换)   #拖动事件发生时，调用红色变换函数，这个函数会接受鼠标的坐标。
 listen()
 
 
